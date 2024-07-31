f0f840e3c3680e43e40fda6571411b6e524519b3
Derek Shore <des2037@med.cornell.edu>
2024-07-31 11:51:12 -0400

    better error response

--- submit_swarm_subjobs.sh
+++ submit_swarm_subjobs.sh
@@ -13,47 +13,39 @@ swarm_number_padded=`printf %04d $swarm_number`
 CWD=`pwd`
 swarm_path=$CWD/raw_swarms/swarm${swarm_number_padded}
 
-
-numberOfFinishedRuns=$(find ./raw_swarms/. -name 'amber_run.log' -exec tail -n1 {} \; | grep FINISHED | wc -l)
-subjob_number=0
-isPriorRun=$(ls ${CWD}/raw_swarms/swarm${swarm_number_padded}/swarm${swarm_number_padded}_traj0000/*subjob*.mdinfo 2> /dev/null | tail -n1 | wc -l)
-
-if [ $isPriorRun == 1 ]; then
-    full_name=$(ls ${CWD}/raw_swarms/swarm${swarm_number_padded}/swarm${swarm_number_padded}_traj0000/*subjob*.mdinfo 2> /dev/null | tail -n1)
-    padded_subjob_number=${full_name: -11:-7}
-    subjob_number=$((10#$padded_subjob_number))
-    ((subjob_number++))
-fi
-
-if [ $subjob_number -gt 0 ] && [ $numberOfFinishedRuns != $number_of_trajs_per_swarm ]
-then
-  ((subjob_number--))
-  pwd
-  echo "job ${subjob_number}_restarted"
-  touch ./subjob_${subjob_number}_restarted
-
-  padded_old_subjob_number=`printf %04d $subjob_number`
-  old_jobs_started=`find . -name "*subjob${padded_old_subjob_number}.mdout" | wc -l`
-
-  if [ $old_jobs_started -lt $number_of_trajs_per_swarm ]; then
-    scancel $SLURM_JOB_ID
-    exit 1
-  fi
-
-  find ${CWD}/raw_swarms/swarm${swarm_number_padded} -name "*subjob${padded_old_subjob_number}*" -exec rm {} \;
-fi
-
-
-
 for ((traj_number=0; traj_number<$number_of_trajs_per_swarm; traj_number++)); do
     traj_number_padded=`printf %04d $traj_number`
     traj_path=$swarm_path/swarm${swarm_number_padded}_traj$traj_number_padded
 
-    cd $traj_path
+    cd $traj_path 
+
+    subjob_number=0
+    isPriorRun=$(ls *subjob*.mdinfo 2> /dev/null | tail -n1 | wc -l)
+
+    if [ $isPriorRun -eq 1 ]; then
+        full_name=$(ls *subjob*.mdinfo 2> /dev/null | tail -n1)
+        padded_subjob_number=${full_name: -11:-7}
+        subjob_number=$((10#$padded_subjob_number))
+        ((subjob_number++))
+    else
+        echo "FINISHED" >> amber_run.log
+    fi
+
+    numberOfFinishedRuns=$(find . -name 'amber_run.log' -exec tail -n1 {} \; | grep FINISHED | wc -l)
+
+    if [ $numberOfFinishedRuns -ne 1 ]; then
+        ((subjob_number--))
+        pwd
+        echo "job ${subjob_number}_restarted"
+        touch ./subjob_${subjob_number}_restarted
+
+        padded_old_subjob_number=`printf %04d $subjob_number`
+        find . -name "*subjob${padded_old_subjob_number}*" -exec rm {} \;
+    fi
 
     OMP_NUM_THREADS=1 srun -u --gpus-per-task=$number_of_gpus_per_replica --gpu-bind=closest -N1 -n1 -c1 ./run_amber.sh $subjob_number > ./amber_log.txt &
 
-    sleep 0.75
+    sleep 0.1
 done
 
 wait
